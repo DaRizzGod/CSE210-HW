7ad87302369d47b29b4f0aa4e4fc1ae9b5c29b19
Miles Josephson <jos21004@byui.edu>
2024-05-02 22:08:45 -0600

    Here is the final form.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
-}
+        Console.WriteLine("What is the Magical Number");
+        int magicNumber = int.Parse(Console.ReadLine());
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,101);
+    
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What number are you guessing? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("A lil Lower");
+            }
+            else
+            {
+                Console.WriteLine("Wahoooo!!!");
+            }
+        }
+}   }
+
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Reflection;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program");
+    }
+    static string PromptUserName ()
+    {
+        Console.WriteLine("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+    static int PromptUserNumber ()
+    {
+        Console.WriteLine("Please enter your favorite number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        return number;
+    }
+    static int SquareNumber (int number)
+    {
+       return number * number; 
+    }
+
+    
+    static void DsiplayResult(string userName, int squaredNumber )
+    {
+        Console.WriteLine($"{userName}, The square that you told me was {squaredNumber}");
+    } 
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int number = PromptUserNumber();
+        int squaredNumber = SquareNumber(number);
+        DsiplayResult(userName, squaredNumber);
     }
-}
+}   
+
+// PromptUserNumber - Asks for and returns the user's favorite number (as an integer)
+// SquareNumber - Accepts an integer as a parameter and returns that number squared (as an integer)
